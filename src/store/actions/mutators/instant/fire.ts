275ce8c59415f41d7490362aadeb35cb0bba6b72
Rolando Wu <rolando.che.wu@gmail.com>
2024-08-15 03:35:55 -0700

    Fixed fire not removing from state.dice

--- src/store/actions/mutators/instant/fire.ts
+++ src/store/actions/mutators/instant/fire.ts
@@ -4,8 +4,15 @@ import {getDice} from '../../getDice';
 export const fire = (state: State, {die}: {die: DieId}) => {
   const existing = state.dieById[die].effect;
 
+  // TODO: Consolidate removal logic to be more modular
+
+  // Off the effects' bookkeeping
   const dice = getDice(state, existing);
   dice[dice.indexOf(die)] = null;
+
+  // Off the active dice bookkeeping
+  const index = state.dice.indexOf(die);
+  state.dice.splice(index, 1);
 
   delete state.dieById[die];
 
