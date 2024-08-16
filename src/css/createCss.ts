a23a5b6bf963eef5631384fbd831da5cdf5cde4c
Rolando Wu <rolando.che.wu@gmail.com>
2024-08-16 02:09:57 -0700

    Improved createCss

--- src/css/createCss.ts
+++ src/css/createCss.ts
@@ -1,49 +1,51 @@
-export const createCss = <T extends Record<string, any>>(styles: T) => {
-  const proxify = <U extends Record<string, any>>(obj: U, path = ''): U => {
-    return new Proxy(obj, {
-      get(target, prop: string) {
-        const newPath = path ? `${path}-${prop}` : prop;
-
-        if (
-          typeof target[prop as keyof U] === 'object' &&
-          target[prop as keyof U] !== null
-        ) {
-          return proxify(target[prop as keyof U], newPath);
-        } else {
-          const name = `--${newPath}`;
-
-          return `var(${name})`;
-        }
-      },
-      set(target, prop: string, value: any) {
-        if (typeof prop === 'symbol') return false;
-
-        const newPath = path ? `${path}-${prop}` : prop;
-        const name = `--${newPath}`;
-
-        // Update the CSS variable value in the DOM
-        document.documentElement.style.setProperty(name, value);
-        target[prop as keyof U] = value;
-        return true;
-      },
-    }) as U;
-  };
-
+export const createCss = <Styles extends Record<string, any>>(
+  styles: Styles,
+) => {
+  // Record of CSS variable and initial value, spread this at :root to register
   const record: Record<string, string> = {};
 
-  const traverse = (obj: Record<string, any>, path = '') => {
-    for (const key in obj) {
-      const newPath = path ? `${path}-${key}` : key;
-      if (typeof obj[key] === 'object' && obj[key] !== null) {
-        traverse(obj[key], newPath);
+  const proxify = <Node extends Record<string, any>>(node: Node, path = '') => {
+    const proxies: Record<keyof Node, Node[string]> = {} as never;
+
+    const getName = (prop: string) => {
+      return path ? `${path}-${prop}` : prop;
+    };
+
+    for (const prop in node) {
+      const value = node[prop];
+
+      const newPath = getName(prop);
+
+      if (typeof value === 'object') {
+        proxies[prop] = proxify(value, newPath);
       } else {
-        const name = `--${newPath}`;
-        record[name] = obj[key];
+        record[`--${newPath}`] = value;
       }
     }
-  };
 
-  traverse(styles);
+    return new Proxy<Node>(node, {
+      // Gets a variable function call instead of the contents in it
+      // Could do another version that returns the contents of it. Maybe return var and val
+      get(target, prop: string) {
+        if (typeof target[prop] === 'object') {
+          return proxies[prop];
+        }
+
+        return `var(--${getName(prop)})`;
+      },
+      // Sets the value of the CSS variable for super easy app wide changes
+      set(target, prop: string, newValue: any) {
+        if (typeof target[prop] === 'object') {
+          // Disallow setting when it returns object
+          return false;
+        }
+
+        document.documentElement.style.setProperty(getName(prop), newValue);
+        Reflect.set(node, prop, newValue);
+        return true;
+      },
+    });
+  };
 
   const vars = proxify(styles);
 
