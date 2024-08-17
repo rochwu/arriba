827658cbba59ca7df720685dcc49513556722a47
Rolando Wu <rolando.che.wu@gmail.com>
2024-08-17 01:10:06 -0700

    Better box shadows

--- packages/css/src/css.ts
+++ packages/css/src/css.ts
@@ -1,43 +1,38 @@
 import {createCssVars} from './createCssVars';
 
-export const {vars, root} = createCssVars(
-  {
-    backgroundColor: '#deb887',
-    borderRadius: '8px',
-    boxShadowOffset: '2px',
+export const {vars, root} = createCssVars({
+  backgroundColor: '#deb887',
+  borderRadius: '8px',
+  boxShadow: '2px 2px rgba(0, 0, 0, 0.10)',
+  color: 'black',
+  fontFamily: 'Arial, Helvetica, sans-serif',
+  gap: '8px',
+  tooltip: {
+    backgroundColor: '#cdaa7d',
+  },
+  die: {
+    backgroundColor: 'white',
     color: 'black',
-    fontFamily: 'Arial, Helvetica, sans-serif',
-    gap: '8px',
-    tooltip: {
-      backgroundColor: '#cdaa7d',
-    },
-    die: {
-      backgroundColor: 'white',
-      color: 'black',
-      size: '50px',
-      roll: {
-        size: '12px',
-        color: 'white',
-        backgroundColor: 'red',
-        fontWeight: 600,
-      },
-    },
-    slot: {
-      backgroundColor: '#fff8dc',
-      type: {
-        size: '18px',
-        color: 'blue',
-      },
-      min: {
-        // Most come from die.roll
-        color: 'black',
-      },
-    },
-    dot: {
-      size: '8px',
+    size: '50px',
+    roll: {
+      size: '12px',
+      color: 'white',
+      backgroundColor: 'red',
+      fontWeight: 600,
     },
   },
-  (previous) => ({
-    boxShadow: `${previous.boxShadowOffset} ${previous.boxShadowOffset} ${previous.tooltip.backgroundColor}`,
-  }),
-);
+  slot: {
+    backgroundColor: '#fff8dc',
+    type: {
+      size: '18px',
+      color: 'blue',
+    },
+    min: {
+      // Most come from die.roll
+      color: 'black',
+    },
+  },
+  dot: {
+    size: '8px',
+  },
+});
